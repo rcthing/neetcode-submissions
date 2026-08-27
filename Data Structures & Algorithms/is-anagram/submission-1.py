a57f144c18d05fc61ac7c11673c8988c51af3fd9
Rednic Luca <35105055+rcthing@users.noreply.github.com>
2026-08-27 11:28:15 +0300

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        counter = {}
+
+        for char in s:
+            if char in counter:
+                counter[char] += 1
+            else:
+                counter[char] = 1
+        
+        for char in t:
+            if char in counter:
+                counter[char] -= 1
+            else:
+                return False
+
+        for val in counter.values():
+            if val != 0:
+                return False
+                
+        return True
