74da5d8fd2de5cc87c70c768fed3c112f3c4d1db
Rednic Luca <35105055+rcthing@users.noreply.github.com>
2026-09-15 11:19:01 +0300

    Add: search-for-word-ii - submission-9

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-9.py
@@ -0,0 +1,53 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+        self.index = -1
+
+    def addWord(self, word, i):
+        cur = self
+        for ch in word:
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+            cur = cur.children[ch]
+
+        cur.endOfWord = True
+        cur.index = i
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for i,word in enumerate(words):
+            root.addWord(word, i)
+
+        height, lenght = len(board) - 1, len(board[0]) - 1
+        ans = []
+        path = set()
+
+        def dfs(i, j, root):
+            if (i < 0 or j < 0 or i > height or
+             j > lenght or (i, j) in path or
+              board[i][j] not in root.children):
+                return
+
+            cur = root.children[board[i][j]]
+            path.add((i, j))
+
+            if cur.endOfWord and cur.index != -1:
+                ans.append(words[cur.index])
+                cur.index = -1
+
+            dfs(i+1, j, cur)
+            dfs(i-1, j, cur)
+            dfs(i, j+1, cur)
+            dfs(i, j-1, cur)
+
+            path.remove((i, j))
+            return
+        
+        for i in range(height + 1):
+            for j in range(lenght + 1):
+                dfs(i, j, root)
+
+        return ans
