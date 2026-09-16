4447ec816ff69593f80047812acd8140687432a0
Rednic Luca <35105055+rcthing@users.noreply.github.com>
2026-09-16 10:30:03 +0300

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjMap = {i:[] for i in range(numCourses)}
+
+        for crs, pre in prerequisites:
+            if crs == pre:
+                return False
+            adjMap[crs].append(pre)
+        
+        path = set()
+
+        def dfs(crs, pre):
+            if crs in path:
+                return False
+            if pre == []:
+                return True
+            path.add(crs)
+            
+
+            for p in pre:
+                if not dfs(p, adjMap[p]):
+                    return False
+
+            path.remove(crs)
+
+            adjMap[crs] = []
+            return True
+
+        for key in adjMap:
+            if not dfs(key, adjMap[key]):
+                return False
+        return True
+
+        
